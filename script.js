f046c762ef6604814a3760645fd209034213dfa8
428alefaust <alexandre0faustino@gmail.com>
2026-09-24 17:45:37 -0300

    Add initial script.js for widget loading

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Nextcomm - site carregado.");
+
+    /*
+     * O widget da WTS é carregado pelo script
+     * instalado no <head> do index.html.
+     *
+     * Os elementos que possuem:
+     *
+     *     class="h-widget-trigger"
+     *
+     * são utilizados pelo próprio widget
+     * para abrir o atendimento.
+     */
+
+});
